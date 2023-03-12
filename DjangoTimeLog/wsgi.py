bfef7bbd3d045a9fb228ca2947598ac362cab169
dt50 <alexfretka@gmail.com>
2023-03-12 17:40:53 +0300

    Create users app

--- DjangoTimeLog/wsgi.py
+++ DjangoTimeLog/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoTimeLog.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DjangoTimeLog.settings")
 
 application = get_wsgi_application()
